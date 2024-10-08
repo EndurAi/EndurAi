aae3fb4c1b430d128956c1963050fc53209817a5
didi2101 <diego.dasilva@epfl.ch>
2024-10-08 12:19:10 +0200

    Add PreferencesRepository interface for managing preferences

---
+++ app/src/main/java/com/android/sample/model/preferences/PreferencesRepository.kt
@@ -0,0 +1,13 @@
+package com.android.sample.model.preferences
+
+interface PreferencesRepository {
+
+    fun init(onSuccess: () -> Unit)
+
+    fun getPreferences(onSuccess: (Preferences) -> Unit, onFailure: (Exception) -> Unit)
+
+    fun updatePreferences(pref: Preferences, onSuccess: () -> Unit, onFailure: (Exception) -> Unit)
+
+    fun deletePreferences(onSuccess: () -> Unit, onFailure: (Exception) -> Unit)
+
+}
