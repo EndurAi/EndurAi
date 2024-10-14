e956d05471fbda658db6d937a2db6f7a8719b583
Shayan Mouktar <119674649+Shayan105@users.noreply.github.com>
2024-10-14 13:34:38 +0200

    Remove typos

--- app/src/test/java/com/android/sample/ui/navigation/NavigationActionsTest.kt
+++ app/src/test/java/com/android/sample/ui/navigation/NavigationActionsTest.kt
@@ -28,7 +28,7 @@ class NavigationActionsTest {
 
   @Test
   fun navigateToCallsController() {
-    navigationActions.navigateTo(TopLevelDestinations.MAIN) // htht
+    navigationActions.navigateTo(TopLevelDestinations.MAIN) 
     verify(navHostController).navigate(eq(Route.MAIN), any<NavOptionsBuilder.() -> Unit>())
 
     navigationActions.navigateTo(Screen.VIDEO)
