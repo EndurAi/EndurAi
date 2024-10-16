749ab80723a193d99c4dfd635b0b2b08728e6e1e
didi2101 <diego.dasilva@epfl.ch>
2024-10-16 23:22:17 +0200

    Implement Moshi Api for sealed class and remove useless documentName

--- app/src/main/java/com/android/sample/model/workout/Workout.kt
+++ app/src/main/java/com/android/sample/model/workout/Workout.kt
@@ -1,5 +1,8 @@
 package com.android.sample.model.workout
 
+import com.squareup.moshi.JsonClass
+import dev.zacsweers.moshix.sealed.annotations.TypeLabel
+
 abstract class Workout(
     val workoutId: String, // Uniquely identifies the workout among all others
     val name: String,
@@ -8,12 +11,17 @@ abstract class Workout(
     val userIdSet:
         MutableSet<String> // Set of userId that represent the User linked to a specific workout
 ) {
-  abstract val documentName: String
 }
 
 // Detail of the exercise based on its type
+@JsonClass(generateAdapter = true, generator = "sealed:type")
 sealed class ExerciseDetail {
+
+    @TypeLabel("TimeBased")
+    @JsonClass(generateAdapter = true)
   data class TimeBased(val durationInSeconds: Int, val sets: Int) : ExerciseDetail()
 
+    @TypeLabel("RepetitionBased")
+    @JsonClass(generateAdapter = true)
   data class RepetitionBased(val repetitions: Int) : ExerciseDetail()
 }
