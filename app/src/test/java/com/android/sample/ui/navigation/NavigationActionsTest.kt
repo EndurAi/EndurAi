d1e02cad0ee142d89266b0fafbf8f4edf8ec4618
Shayan105 <119674649+Shayan105@users.noreply.github.com>
2024-10-16 14:00:47 +0200

    Add Navigation tests for clearing the BackStack

--- app/src/test/java/com/android/sample/ui/navigation/NavigationActionsTest.kt
+++ app/src/test/java/com/android/sample/ui/navigation/NavigationActionsTest.kt
@@ -1,6 +1,7 @@
 package com.android.sample.ui.navigation
 
 import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph
 import androidx.navigation.NavHostController
 import androidx.navigation.NavOptionsBuilder
 import org.hamcrest.CoreMatchers.`is`
@@ -11,6 +12,7 @@ import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 import org.mockito.Mockito.`when`
 import org.mockito.kotlin.any
+import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.eq
 
 class NavigationActionsTest {
@@ -22,7 +24,11 @@ class NavigationActionsTest {
   @Before
   fun setUp() {
     navigationDestination = mock(NavDestination::class.java)
+    val navGraph = mock(NavGraph::class.java)
+    `when`(navGraph.startDestinationId).thenReturn(0) // Mock the startDestinationId
     navHostController = mock(NavHostController::class.java)
+    `when`(navHostController.graph)
+        .thenReturn(navGraph) // Set the mocked NavGraph to the NavHostController
     navigationActions = NavigationActions(navHostController)
   }
 
@@ -47,5 +53,36 @@ class NavigationActionsTest {
     `when`(navigationDestination.route).thenReturn(Route.MAIN)
 
     assertThat(navigationActions.currentRoute(), `is`(Route.MAIN))
+  }
+
+  @Test
+  fun navigateToTopLevelDestinationClearsBackStack() {
+    val destination = TopLevelDestinations.MAIN
+    navigationActions.navigateTo(destination)
+
+    val captor = argumentCaptor<NavOptionsBuilder.() -> Unit>()
+    verify(navHostController).navigate(eq(destination.route), captor.capture())
+
+    val navOptionsBuilder = NavOptionsBuilder()
+    captor.firstValue.invoke(navOptionsBuilder)
+    assertThat(navOptionsBuilder.popUpTo, `is`(0))
+    assertThat(navOptionsBuilder.launchSingleTop, `is`(true))
+    assertThat(navOptionsBuilder.restoreState, `is`(true))
+  }
+
+  @Test
+  fun navigateToTopLevelDestinationClearsBackStackWithAUTH_PATH() {
+    val destinationMocked = mock(TopLevelDestination::class.java)
+    `when`(destinationMocked.route).thenReturn(Route.AUTH)
+    navigationActions.navigateTo(destinationMocked)
+
+    val captor = argumentCaptor<NavOptionsBuilder.() -> Unit>()
+    verify(navHostController).navigate(eq(destinationMocked.route), captor.capture())
+
+    val navOptionsBuilder = NavOptionsBuilder()
+    captor.firstValue.invoke(navOptionsBuilder)
+    assertThat(navOptionsBuilder.popUpTo, `is`(0))
+    assertThat(navOptionsBuilder.launchSingleTop, `is`(true))
+    assertThat(navOptionsBuilder.restoreState, `is`(false))
   }
 }
