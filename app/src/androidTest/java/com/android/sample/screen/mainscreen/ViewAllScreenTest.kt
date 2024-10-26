f750852cd363bbe2afd93d9d7c801df0a9fd41c9
Alec <alec.trachsel@epfl.ch>
2024-10-26 22:07:21 +0200

    Add some checks for the workouts card for bodyweight and yoga

--- app/src/androidTest/java/com/android/sample/screen/mainscreen/ViewAllScreenTest.kt
+++ app/src/androidTest/java/com/android/sample/screen/mainscreen/ViewAllScreenTest.kt
@@ -2,7 +2,6 @@ package com.android.sample.ui.mainscreen
 
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import com.android.sample.R
 import com.android.sample.model.workout.BodyWeightWorkout
 import com.android.sample.model.workout.WorkoutRepository
 import com.android.sample.model.workout.WorkoutViewModel
@@ -16,60 +15,61 @@ import org.mockito.kotlin.any
 
 class ViewAllScreenTest {
 
-    @get:Rule
-    val composeTestRule = createComposeRule()
+  @get:Rule val composeTestRule = createComposeRule()
 
-    private lateinit var bodyWeightViewModel: WorkoutViewModel<BodyWeightWorkout>
-    private lateinit var yogaViewModel: WorkoutViewModel<YogaWorkout>
-    private lateinit var navigationActions: NavigationActions
-    private lateinit var bodyWeightRepo: WorkoutRepository<BodyWeightWorkout>
-    private lateinit var yogaRepo: WorkoutRepository<YogaWorkout>
+  private lateinit var bodyWeightViewModel: WorkoutViewModel<BodyWeightWorkout>
+  private lateinit var yogaViewModel: WorkoutViewModel<YogaWorkout>
+  private lateinit var navigationActions: NavigationActions
+  private lateinit var bodyWeightRepo: WorkoutRepository<BodyWeightWorkout>
+  private lateinit var yogaRepo: WorkoutRepository<YogaWorkout>
 
+  @Before
+  fun setUp() {
+    bodyWeightRepo = mock()
+    yogaRepo = mock()
 
-    @Before
-    fun setUp() {
-        bodyWeightRepo = mock()
-        yogaRepo = mock()
-
-        val bodyWeightWorkouts = listOf(
+    val bodyWeightWorkouts =
+        listOf(
             BodyWeightWorkout("1", "NopainNogain", "Do 20 push-ups", false),
-            BodyWeightWorkout("2", "NightSes", "Hold for 60 seconds", false)
-        )
-        val yogaWorkouts = listOf(
-            YogaWorkout("3", "Sunrise", "Perform morning yoga routine", false),
-            YogaWorkout("4", "Relax", "Hold the pose for balance", false)
-        )
+            BodyWeightWorkout("2", "NightSes", "Hold for 60 seconds", false))
+    val yogaWorkouts: List<YogaWorkout> = listOf()
 
-        `when`(bodyWeightRepo.getDocuments(any(), any())).thenAnswer {
-            it.getArgument<(List<BodyWeightWorkout>) -> Unit>(0)(bodyWeightWorkouts)
-        }
-
-        `when`(yogaRepo.getDocuments(any(), any())).thenAnswer {
-            it.getArgument<(List<YogaWorkout>) -> Unit>(0)(yogaWorkouts)
-        }
-
-        bodyWeightViewModel = WorkoutViewModel(bodyWeightRepo)
-        yogaViewModel = WorkoutViewModel(yogaRepo)
-        navigationActions = mock(NavigationActions::class.java)
-
-        composeTestRule.setContent {
-            ViewAllScreen(
-                navigationActions = navigationActions,
-                bodyWeightViewModel = bodyWeightViewModel,
-                yogaViewModel = yogaViewModel
-            )
-        }
+    `when`(bodyWeightRepo.getDocuments(any(), any())).then {
+      it.getArgument<(List<BodyWeightWorkout>) -> Unit>(0)(bodyWeightWorkouts)
     }
 
-    @Test
-    fun testDisplayAllTags() {
-        composeTestRule.onNodeWithTag("ViewAllScreen").assertIsDisplayed()
-
-        composeTestRule.onNodeWithTag("ScreenTitle").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("ArrowBackButton").assertIsDisplayed()
-
-        composeTestRule.onNodeWithTag("BodyTab").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("YogaTab").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("RunningTab").assertIsDisplayed()
+    `when`(yogaRepo.getDocuments(any(), any())).then {
+      it.getArgument<(List<YogaWorkout>) -> Unit>(0)(yogaWorkouts)
     }
+
+    bodyWeightViewModel = WorkoutViewModel(bodyWeightRepo)
+    yogaViewModel = WorkoutViewModel(yogaRepo)
+
+    navigationActions = mock(NavigationActions::class.java)
+
+    composeTestRule.setContent {
+      ViewAllScreen(
+          navigationActions = navigationActions,
+          bodyWeightViewModel = bodyWeightViewModel,
+          yogaViewModel = yogaViewModel)
+    }
+  }
+
+  @Test
+  fun testDisplayAllTags() {
+    bodyWeightViewModel.getWorkouts()
+    yogaViewModel.getWorkouts()
+    composeTestRule.onNodeWithTag("ViewAllScreen").assertIsDisplayed()
+
+    composeTestRule.onNodeWithTag("ScreenTitle").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("ArrowBackButton").assertIsDisplayed()
+
+    composeTestRule.onNodeWithTag("BodyTab").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("YogaTab").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("RunningTab").assertIsDisplayed()
+    composeTestRule.onAllNodesWithTag("WorkoutCard").assertCountEquals(2)
+
+    composeTestRule.onNodeWithTag("YogaTab").performClick()
+    composeTestRule.onNodeWithTag("emptyWorkoutPrompt").assertIsDisplayed()
+  }
 }
