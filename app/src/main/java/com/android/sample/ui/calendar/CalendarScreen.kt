95032b46be145ccaf1477866539cf4b7afdd7f0e
didi2101 <diego.dasilva@epfl.ch>
2024-10-31 20:19:05 +0100

    Implement a system of color to distinguish the two types of workout.
    And all colours have been taken from color.kt

--- app/src/main/java/com/android/sample/ui/calendar/CalendarScreen.kt
+++ app/src/main/java/com/android/sample/ui/calendar/CalendarScreen.kt
@@ -1,7 +1,9 @@
 package com.android.sample.ui.calendar
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.ui.Alignment
@@ -11,15 +13,16 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.android.sample.model.workout.Workout
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import com.android.sample.model.workout.WorkoutViewModel
 import com.android.sample.ui.navigation.NavigationActions
@@ -30,24 +33,45 @@ import kotlinx.datetime.plus
 import kotlinx.datetime.toKotlinLocalDate
 import kotlinx.datetime.toLocalDateTime
 import androidx.compose.runtime.*
+import com.android.sample.ui.composables.TopBar
 import java.time.LocalDateTime
+import com.android.sample.R
+import com.android.sample.model.workout.WorkoutType
+import com.android.sample.ui.theme.Black
+import com.android.sample.ui.theme.DarkGray
+import com.android.sample.ui.theme.Gray
+import com.android.sample.ui.theme.LightGray
+import com.android.sample.ui.theme.PastelBlue
+import com.android.sample.ui.theme.PastelRed
+import com.android.sample.ui.theme.Red
+import com.android.sample.ui.theme.Yellow
 
 
-@SuppressLint("StateFlowValueCalledInComposition")
+data class ColoredWorkout(
+    val workout: Workout,
+    val backgroundColor: Color,
+    val type : WorkoutType
+)
+
 @Composable
 fun CalendarScreen(
     navigationActions: NavigationActions,
     bodyworkoutViewModel: WorkoutViewModel<Workout>,
     yogaworkoutViewModel: WorkoutViewModel<Workout>
 ) {
-    val workoutsBody = bodyworkoutViewModel.workouts.value
-    val workoutsYoga = yogaworkoutViewModel.workouts.value
-    val workouts = workoutsYoga + workoutsBody
+    val workoutsBody by bodyworkoutViewModel.workouts.collectAsState(emptyList())
+    val workoutsYoga by yogaworkoutViewModel.workouts.collectAsState(emptyList())
+
+    val coloredWorkoutsBody = workoutsBody.map { ColoredWorkout(it, PastelBlue, WorkoutType.BODY_WEIGHT) } // Rouge pastel
+    val coloredWorkoutsYoga = workoutsYoga.map { ColoredWorkout(it, PastelRed, WorkoutType.YOGA) } // Bleu pastel
+    val workouts = coloredWorkoutsYoga + coloredWorkoutsBody
 
     // Infinite scrolling logic
     val lazyListState = rememberLazyListState()
-    val coroutineScope = rememberCoroutineScope()
     var daysToShow by remember { mutableStateOf(7) } // Start with 7 days
+
+    var selectedWorkout by remember { mutableStateOf<ColoredWorkout?>(null) }
+    var showDialog by remember { mutableStateOf(false) }
 
     val startDate = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
     val dates = generateDateRange(startDate, daysToShow)
@@ -57,31 +81,97 @@ fun CalendarScreen(
         snapshotFlow { lazyListState.firstVisibleItemIndex + lazyListState.layoutInfo.visibleItemsInfo.size >= lazyListState.layoutInfo.totalItemsCount }
             .collect { isAtEnd ->
                 if (isAtEnd) {
-                    println("Reached the end of the list")
                     daysToShow += 7
                 }
             }
     }
 
+    if (showDialog && selectedWorkout != null) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ){Text("Workout Actions") }},
+            confirmButton = {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
 
+                    OutlinedButton(
+                        onClick = {
+                            showDialog = false
+                        },
+                        colors = ButtonDefaults.outlinedButtonColors(),
+                        border = BorderStroke(2.dp, Yellow)
+                    ) {
+                        Icon(Icons.Default.Edit, contentDescription = "Edit", tint = Yellow)
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text("Edit", color = Black)
+                    }
 
-
-    // Group workouts by date
-    val workoutsByDate = workouts.groupBy {
-        it.date.toLocalDate().toKotlinLocalDate()
+                    OutlinedButton(
+                        onClick = {
+                            when    (selectedWorkout!!.type){
+                                WorkoutType.BODY_WEIGHT -> bodyworkoutViewModel.deleteWorkoutById(selectedWorkout!!.workout.workoutId)
+                                WorkoutType.YOGA -> yogaworkoutViewModel.deleteWorkoutById(selectedWorkout!!.workout.workoutId)
+                                WorkoutType.RUNNING -> {}
+                            }
+                            showDialog = false
+                        },
+                        colors = ButtonDefaults.outlinedButtonColors(),
+                        border = BorderStroke(2.dp, Red)
+                    ) {
+                        Icon(Icons.Default.Delete, contentDescription = "Delete", tint = Red)
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text("Delete", color = Black)
+                    }
+                }
+            }
+        )
     }
 
-    LazyColumn(
-        state = lazyListState,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        items(dates) { date ->
-            DaySection(
-                date = date,
-                workouts = workoutsByDate[date] ?: emptyList()
-            )
+
+
+    val workoutsByDate = workouts.groupBy {
+        it.workout.date.toLocalDate().toKotlinLocalDate()
+    }
+
+    Scaffold(
+        topBar = { TopBar(navigationActions, R.string.calendar_tittle) }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Legend()
+
+
+            LazyColumn(
+                state = lazyListState,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                items(dates) { date ->
+                    DaySection(
+                        date = date,
+                        workouts = workoutsByDate[date] ?: emptyList(),
+                        onWorkoutClick = {workout -> selectedWorkout = workout
+                            showDialog = true}
+                    )
+                }
+            }
         }
     }
 }
@@ -91,22 +181,51 @@ private fun generateDateRange(startDate: kotlinx.datetime.LocalDate, count: Int)
 }
 
 @Composable
-fun LazyListState.isScrolledToEnd(): Boolean {
-    val layoutInfo = remember(this) { this.layoutInfo }
-    return layoutInfo.visibleItemsInfo.lastOrNull()?.index == layoutInfo.totalItemsCount - 1
+fun Legend() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 2.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        LegendItem(color = PastelBlue, label = "Yoga")
+
+        LegendItem(color = PastelRed, label = "Bodyweight")
+    }
+}
+
+@Composable
+fun LegendItem(color: Color, label: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(16.dp)
+                .background(color, shape = MaterialTheme.shapes.small)
+        )
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodySmall
+        )
+    }
 }
 
 @Composable
 fun DaySection(
     date: kotlinx.datetime.LocalDate,
-    workouts: List<Workout>
+    workouts: List<ColoredWorkout>,
+    onWorkoutClick: (ColoredWorkout) -> Unit
 ) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp)
             .background(
-                color = Color.LightGray.copy(alpha = 0.3f),
+                color = LightGray.copy(alpha = 0.3f),
                 shape = MaterialTheme.shapes.medium
             )
             .padding(16.dp),
@@ -127,7 +246,7 @@ fun DaySection(
                 text = getMonthName(date.monthNumber),
                 style = MaterialTheme.typography.bodySmall,
                 fontWeight = FontWeight.Light,
-                color = Color.DarkGray
+                color = DarkGray
             )
         }
 
@@ -135,15 +254,15 @@ fun DaySection(
             Text(
                 text = "Pas d'entraînement",
                 style = MaterialTheme.typography.bodyMedium,
-                color = Color.Gray
+                color = Gray
             )
         } else {
             Column(
                 modifier = Modifier,
                 horizontalAlignment = Alignment.End
             ) {
-                workouts.sortedBy { it.date }.forEach { workout ->
-                    WorkoutItem(workout)
+                workouts.sortedBy { it.workout.date }.forEach { workout ->
+                    WorkoutItem(workout,onWorkoutClick)
                 }
             }
         }
@@ -152,14 +271,16 @@ fun DaySection(
 
 @Composable
 fun WorkoutItem(
-    workout: Workout
+    coloredWorkout: ColoredWorkout,
+    onClick: (ColoredWorkout) -> Unit
 ) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
+            .clickable { onClick(coloredWorkout) }
             .padding(vertical = 4.dp),
         colors = CardDefaults.cardColors(
-            containerColor = Color(0xFFE3F2FD)
+            containerColor = coloredWorkout.backgroundColor
         ),
         shape = MaterialTheme.shapes.medium
     ) {
@@ -171,11 +292,11 @@ fun WorkoutItem(
             verticalAlignment = Alignment.CenterVertically
         ) {
             Text(
-                text = workout.name,
+                text = coloredWorkout.workout.name,
                 style = MaterialTheme.typography.bodyLarge
             )
             Text(
-                text = formatTime(workout.date),
+                text = formatTime(coloredWorkout.workout.date),
                 style = MaterialTheme.typography.bodyLarge
             )
         }
