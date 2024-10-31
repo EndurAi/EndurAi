e036a82886e9e62e8b71ae2c979c8eca4644d812
Shayan105 <119674649+Shayan105@users.noreply.github.com>
2024-10-31 17:45:59 +0100

    FInish the UI

--- app/src/main/java/com/android/sample/ui/composables/CountDownTimer.kt
+++ app/src/main/java/com/android/sample/ui/composables/CountDownTimer.kt
@@ -1,46 +1,34 @@
 package com.android.sample.ui.composables
 
-import android.content.res.Resources
-import android.graphics.BitmapFactory
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Outline
-import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.drawText
-
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.android.sample.R
-import kotlin.math.cos
-import kotlin.math.sin
+
 @Composable
 @Preview
 fun CountDownTimer(
-  currentTime_str: String = "00:02",
-  maxTime_str: String ="00:03",
-  fullColor: Color = Color.Green,
-  criticalColor: Color = Color.Red,
-  criticalRate: Float = 0.1f,
-  stroke: Int = 28,
-  arcSize: Float = 500f,
-  onFinishedText : String ="Done",
-  onFinishedIcon : ImageVector= Icons.Filled.Done
+    currentTime_str: String = "00:02",
+    maxTime_str: String = "00:03",
+    fullColor: Color = Color.Green,
+    criticalColor: Color = Color.Red,
+    criticalRate: Float = 0.15f,
+    stroke: Int = 28,
+    arcSize: Float = 500f,
+    onFinishedText: String = "Done",
+    onFinishedIcon: ImageVector = Icons.Filled.Done
 ) {
   Canvas(modifier = Modifier.fillMaxSize()) {
     val currentTime = convertTimeToSeconds(currentTime_str)
@@ -51,84 +39,79 @@ fun CountDownTimer(
 
     val angle = (currentTime.toFloat() / maxTime.toFloat()) * 360f
 
-    // Drawing the small black circle at the center
-    drawCircle(
-      color = if(currentTime >0) Color.Black else Color.Green,
-      radius = (arcSize / 2) * 0.94F, // Radius of the small circle; adjust this as needed
-      center = Offset(width / 2, height / 2),
-      style = Stroke(width = 5F)
-    )
+    // Drawing the small  circle at the center
+    drawArc(
+        color =
+            if (currentTime > 0) Color(red = 223, green = 162, blue = 240, alpha = 100)
+            else Color.Green,
+        topLeft = Offset((width - arcSize) / 2, (height - arcSize) / 2),
+        startAngle = 0F,
+        sweepAngle = 360F,
+        useCenter = false,
+        style = Stroke(stroke.toFloat(), cap = StrokeCap.Round),
+        size = Size(arcSize, arcSize))
 
     // Drawing the arc
     drawArc(
-      color = if (currentTime.toFloat() / maxTime.toFloat() < criticalRate) criticalColor else fullColor,
-      topLeft = Offset((width - arcSize) / 2, (height - arcSize) / 2),
-      startAngle = -90f,
-      sweepAngle = angle,
-      useCenter = false,
-      style = Stroke(stroke.toFloat(), cap = StrokeCap.Round),
-      size = Size(arcSize, arcSize)
-    )
+        color =
+            if (currentTime.toFloat() / maxTime.toFloat() < criticalRate) criticalColor
+            else fullColor,
+        topLeft = Offset((width - arcSize) / 2, (height - arcSize) / 2),
+        startAngle = -90f,
+        sweepAngle = angle,
+        useCenter = false,
+        style = Stroke(stroke.toFloat(), cap = StrokeCap.Round),
+        size = Size(arcSize, arcSize))
 
     // Centering the text within the arc
     drawIntoCanvas { canvas ->
-      val paint = android.graphics.Paint().apply {
-        color = android.graphics.Color.BLACK
-        textAlign = android.graphics.Paint.Align.CENTER
-        textSize = 120f
-        isAntiAlias = true // Enable anti-aliasing
-      }
-      if(currentTime > 0) {
+      val paint =
+          android.graphics.Paint().apply {
+            color = android.graphics.Color.BLACK
+            textAlign = android.graphics.Paint.Align.CENTER
+            textSize = 120f
+            isAntiAlias = true // Enable anti-aliasing
+          }
+      if (currentTime > 0) {
         canvas.nativeCanvas.drawText(
-          currentTime_str,
-          width / 2,
-          height / 2 + 32f, // Centering text along the circle’s vertical axis
-          paint
-        )
-      }
-      else{
+            currentTime_str,
+            width / 2,
+            height / 2 + 32f, // Centering text along the circle’s vertical axis
+            paint)
+      } else {
 
         val checkStartX = width / 2 - 60f
         val checkStartY = height / 2 - 80f // Moved UPWARDS
         val checkLineLength = 80f
 
-        val checkPaint = android.graphics.Paint().apply {
-          color = android.graphics.Color.GREEN
-          strokeWidth = 23f
-          strokeCap = android.graphics.Paint.Cap.ROUND
-          isAntiAlias = true
-        }
+        val checkPaint =
+            android.graphics.Paint().apply {
+              color = android.graphics.Color.GREEN
+              strokeWidth = 23f
+              strokeCap = android.graphics.Paint.Cap.ROUND
+              isAntiAlias = true
+            }
 
         canvas.nativeCanvas.drawLine(
-          checkStartX ,
-          checkStartY + 30f,
-          checkStartX + checkLineLength * 0.6f,
-          checkStartY + checkLineLength+45,
-          checkPaint
-        )
+            checkStartX,
+            checkStartY + 30f,
+            checkStartX + checkLineLength * 0.6f,
+            checkStartY + checkLineLength + 45,
+            checkPaint)
         canvas.nativeCanvas.drawLine(
-          checkStartX + checkLineLength * 0.6f,
-          checkStartY + checkLineLength+50,
-          checkStartX + checkLineLength + 60f,
-          checkStartY - checkLineLength * 0.7f,
-          checkPaint
-        )
+            checkStartX + checkLineLength * 0.6f,
+            checkStartY + checkLineLength + 50,
+            checkStartX + checkLineLength + 60f,
+            checkStartY - checkLineLength * 0.7f,
+            checkPaint)
 
         // Draw the "Done!" text BELOW the check mark
         canvas.nativeCanvas.drawText(
-          onFinishedText,
-          width / 2,
-          height / 2 + 150f, // Moved DOWNWARDS
-          paint
-        )
-
-
-
-
+            onFinishedText,
+            width / 2,
+            height / 2 + 150f, // Moved DOWNWARDS
+            paint)
       }
-
-
-
     }
   }
 }
--- app/src/main/java/com/android/sample/ui/workout/WarmUpScreen.kt
+++ app/src/main/java/com/android/sample/ui/workout/WarmUpScreen.kt
@@ -2,6 +2,7 @@ package com.android.sample.ui.workout
 
 import android.content.Context
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -14,15 +15,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -35,7 +32,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -51,37 +47,43 @@ import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
 import com.android.sample.R
+import com.android.sample.model.workout.BodyWeightWorkout
+import com.android.sample.model.workout.WorkoutViewModel
 import com.android.sample.ui.composables.CountDownTimer
 import com.android.sample.ui.composables.convertTimeToSeconds
+import com.android.sample.ui.navigation.NavigationActions
 import kotlinx.coroutines.delay
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun ExerciseScreen(
-    exerciseName: String = "Jumping Jacks",
-    instruction: String = "Stand, jump while spreading arms and legs.",
+    navigationActions: NavigationActions,
+    bodyWeightViewModel: WorkoutViewModel<BodyWeightWorkout>,
+    yogaViewModel: WorkoutViewModel<Yog>
+
     repetitions: Int = 10,
     onSkip: () -> Unit = {},
     onStart: () -> Unit = {}
 ) {
+  // Variable for the screen
   var videoBoxIsDisplayed by remember { mutableStateOf(false) }
   var finishButtonBoxIsDisplayed by remember { mutableStateOf(true) }
   var presentationButtonBoxIsDisplayed by remember { mutableStateOf(false) }
   var goalCounterBoxIsDisplayed by remember { mutableStateOf(true) }
-  val timeLimit = "02:45"
+  var countDownTimerIsPaused by remember { mutableStateOf(false) }
+  // Hard coded values
+  val timeLimit = "00:10"
   val timer = remember { mutableStateOf(timeLimit) }
   val exerciseIsRepetitionBased = false
   LaunchedEffect(Unit) {
-
     var totalSeconds = convertTimeToSeconds(timer.value)
     while (totalSeconds >= 0) {
       val minutes = totalSeconds / 60
       val seconds = totalSeconds % 60
       timer.value = String.format("%02d:%02d", minutes, seconds)
       delay(1000L)
-      if (goalCounterBoxIsDisplayed ) {
+      if (goalCounterBoxIsDisplayed && !countDownTimerIsPaused) {
         totalSeconds--
       }
     }
@@ -101,119 +103,153 @@ fun ExerciseScreen(
                   color = MaterialTheme.colorScheme.onSurface)
             })
       }) { innerPadding ->
-    Column(
-      modifier =
-      Modifier.fillMaxSize()
-        .padding(innerPadding) // Use innerPadding to avoid overlapping with the app bar
-        .padding(16.dp),
-      horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-      Text(
-        text = exerciseName,
-        style = MaterialTheme.typography.labelLarge.copy(fontSize = 35.sp),
-        fontWeight = FontWeight.Bold,
-        modifier = Modifier.height(79.dp)
-      )
+        Column(
+            modifier =
+                Modifier.fillMaxSize()
+                    .padding(innerPadding) // Use innerPadding to avoid overlapping with the app bar
+                    .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+              Text(
+                  text = exerciseName,
+                  style = MaterialTheme.typography.labelLarge.copy(fontSize = 35.sp),
+                  fontWeight = FontWeight.Bold,
+                  modifier = Modifier.height(79.dp))
 
-      Spacer(modifier = Modifier.height(16.dp))
-      Text(
-        text = instruction,
-        style =
-        MaterialTheme.typography.displaySmall.copy(
-          fontSize = 20.sp, lineHeight = 25.sp
-        ),
-        textAlign = TextAlign.Center,
-        modifier = Modifier.width(317.dp).height(79.dp)
-      )
-      Spacer(modifier = Modifier.height(16.dp))
-      val URL =
-        "https://firebasestorage.googleapis.com/v0/b/endurai-92811.appspot.com/o/template_videos%2FPush%20Up.mp4?alt=media&token=2677215b-59a4-47c8-854b-a3326532e8af"
+              Spacer(modifier = Modifier.height(16.dp))
+              Text(
+                  text = instruction,
+                  style =
+                      MaterialTheme.typography.displaySmall.copy(
+                          fontSize = 20.sp, lineHeight = 25.sp),
+                  textAlign = TextAlign.Center,
+                  modifier = Modifier.width(317.dp).height(79.dp))
+              Spacer(modifier = Modifier.height(16.dp))
+              val URL =
+                  "https://firebasestorage.googleapis.com/v0/b/endurai-92811.appspot.com/o/template_videos%2FPush%20Up.mp4?alt=media&token=2677215b-59a4-47c8-854b-a3326532e8af"
 
-      // Box for the video player
-      if (videoBoxIsDisplayed) {
-        val videoBox =
-          Box(modifier = Modifier.size(width = 350.dp, height = 200.dp)) {
-            VideoPlayer(context = LocalContext.current, url = URL)
-          }
-      }
+              // Box for the video player
+              if (videoBoxIsDisplayed) {
+                val videoBox =
+                    Box(modifier = Modifier.size(width = 350.dp, height = 200.dp)) {
+                      VideoPlayer(context = LocalContext.current, url = URL)
+                    }
+              }
 
-      Spacer(modifier = Modifier.height(16.dp))
-      Row(verticalAlignment = Alignment.CenterVertically) {
-        Icon(imageVector = Icons.Filled.MoreVert, contentDescription = "Repetitions")
-        Spacer(modifier = Modifier.width(8.dp))
-        Text(text = "$repetitions Rep.")
-      }
-      Spacer(modifier = Modifier.height(35.dp))
+              Spacer(modifier = Modifier.height(16.dp))
+              Row(verticalAlignment = Alignment.CenterVertically) {
+                if (exerciseIsRepetitionBased) {
+                  Image(
+                      painter = painterResource(R.drawable.baseline_timeline_24),
+                      contentDescription = "repetition",
+                      modifier = Modifier.padding(horizontal = (5).dp))
 
-      // Box for the goal counter
-      if (goalCounterBoxIsDisplayed) {
+                  Text(text = "$repetitions Rep.", fontSize = 20.sp)
+                } else {
+                  Image(
+                      painter = painterResource(R.drawable.baseline_access_time_24),
+                      contentDescription = "timerLogo",
+                      modifier = Modifier.padding(horizontal = (5).dp))
+                  Text(timeLimit, fontSize = 20.sp)
+                }
+              }
+              Spacer(modifier = Modifier.height(35.dp))
 
-        if (exerciseIsRepetitionBased) {
-          Box(modifier = Modifier.size(width = 350.dp, height = 200.dp)) {
-            val workoutImage =
-              Image(
-                painter = painterResource(id = R.drawable.warmup_logo),
-                contentDescription = "Goal Counter",
-                modifier = Modifier.size(350.dp, 200.dp)
-              )
-          }
-        } else {
-          CountDownTimer(currentTime_str = timer.value, maxTime_str = timeLimit)
+              // Box for the goal counter
+              if (goalCounterBoxIsDisplayed) {
 
-        }
-      }
+                if (exerciseIsRepetitionBased) {
+                  Box(modifier = Modifier.size(200.dp)) {
+                    val workoutImage =
+                        Image(
+                            painter = painterResource(id = R.drawable.warmup_logo),
+                            contentDescription = "Goal Counter",
+                            modifier = Modifier.size(350.dp, 200.dp))
+                  }
+                } else {
+                  Box(modifier = Modifier.size(200.dp)) {
+                    CountDownTimer(currentTime_str = timer.value, maxTime_str = timeLimit)
+                  }
+                }
+              }
 
+              // Presentation button box
+              if (presentationButtonBoxIsDisplayed) {
+                val presentationButtonBox =
+                    Column(
+                        modifier = Modifier.size(height = 120.dp, width = 180.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Bottom) {
+                          // Skip button
+                          Button(
+                              onClick = { videoBoxIsDisplayed = !videoBoxIsDisplayed },
+                              colors = ButtonDefaults.buttonColors(containerColor = Color.Red)) {
+                                Text("Skip", color = Color.Black)
+                              }
+                          Spacer(modifier = Modifier.height(25.dp))
+                          // Start button
+                          Button(
+                              onClick = {
+                                presentationButtonBoxIsDisplayed = false
+                                goalCounterBoxIsDisplayed = true
+                                finishButtonBoxIsDisplayed = true
+                                videoBoxIsDisplayed = false
+                              },
+                              modifier = Modifier.width(200.dp).height(50.dp),
+                              colors =
+                                  ButtonDefaults.buttonColors(containerColor = Color(0xFFA9B0FF)),
+                              shape = RoundedCornerShape(size = 11.dp)) {
+                                Text("Start", color = Color.Black, fontSize = 20.sp)
+                              }
+                        }
+              } else if (finishButtonBoxIsDisplayed) {
+                val finishButtonBox =
+                    Column(
+                        modifier = Modifier.size(height = 250.dp, width = 180.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Top) {
+                          if (!exerciseIsRepetitionBased && timer.value != "00:00") {
+                            Button(
+                                onClick = { countDownTimerIsPaused = !countDownTimerIsPaused },
+                                modifier = Modifier.size(65.dp)) {
+                                  Image(
+                                      painterResource(
+                                          if (countDownTimerIsPaused)
+                                              R.drawable.baseline_play_arrow_24
+                                          else R.drawable.baseline_pause_24),
+                                      contentDescription = "play/pausebtn",
+                                      modifier = Modifier.fillMaxSize())
+                                }
+                            Spacer(Modifier.size(25.dp))
+                          } else {
+                            Spacer(modifier = Modifier.size(90.dp))
+                          }
 
-      // Presentation button box
-      if (presentationButtonBoxIsDisplayed) {
-        val presentationButtonBox =
-          Column(
-            modifier = Modifier.size(height = 120.dp, width = 180.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Bottom
-          ) {
-            // Skip button
-            Button(
-              onClick = { videoBoxIsDisplayed = !videoBoxIsDisplayed },
-              colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
-            ) {
-              Text("Skip", color = Color.Black)
+                          Spacer(Modifier.size(25.dp))
+                          val finishButton =
+                              Button(
+                                  onClick = {},
+                                  modifier = Modifier.width(200.dp).height(50.dp).padding(),
+                                  colors =
+                                      ButtonDefaults.buttonColors(
+                                          containerColor = Color(0xFFA9B0FF)),
+                                  shape = RoundedCornerShape(size = 11.dp)) {
+                                    Text("Finish", color = Color.Black, fontSize = 20.sp)
+                                  }
+
+                          Spacer(Modifier.size(25.dp))
+
+                          val skipButton =
+                              Button(
+                                  onClick = {},
+                                  colors =
+                                      ButtonDefaults.buttonColors(containerColor = Color.Red)) {
+                                    Text("Skip", color = Color.Black)
+                                  }
+
+                        }
+              }
             }
-            Spacer(modifier = Modifier.height(25.dp))
-            // Start button
-            Button(
-              onClick = {
-                presentationButtonBoxIsDisplayed = false
-                goalCounterBoxIsDisplayed = true
-                finishButtonBoxIsDisplayed = true
-                videoBoxIsDisplayed = false
-              },
-              modifier = Modifier.width(200.dp).height(50.dp),
-              colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFA9B0FF)),
-              shape = RoundedCornerShape(size = 11.dp)
-            ) {
-              Text("Start", color = Color.Black, fontSize = 20.sp)
-            }
-          }
-      } else if (finishButtonBoxIsDisplayed) {
-        val finishButtonBox =
-          Column(
-            modifier = Modifier.size(height = 120.dp, width = 180.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Bottom
-          ) {
-            Button(
-              onClick = {},
-              modifier = Modifier.width(200.dp).height(50.dp),
-              colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFA9B0FF)),
-              shape = RoundedCornerShape(size = 11.dp)
-            ) {
-              Text("Finish", color = Color.Black, fontSize = 20.sp)
-            }
-          }
       }
-    }
-  }
 }
 
 @androidx.annotation.OptIn(UnstableApi::class)
