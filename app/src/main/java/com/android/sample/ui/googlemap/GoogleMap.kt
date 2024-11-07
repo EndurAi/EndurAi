93c9f684a6f1103a36cb8f8eeec48be5465d1351
didi2101 <diego.dasilva@epfl.ch>
2024-11-07 21:06:38 +0100

    A simple new class where i test GPS things

---
+++ app/src/main/java/com/android/sample/ui/googlemap/GoogleMap.kt
@@ -0,0 +1,23 @@
+package com.android.sample.ui.googlemap
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.rememberCameraPositionState
+
+@Composable
+fun SimpleMap() {
+    // Create a CameraPositionState to control the camera position
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(LatLng(37.7749, -122.4194), 10f) // San Francisco
+    }
+
+    // Display the Google Map
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    )
+}
