249d903c92a11cf752f99a32de1f293b3e1b294d
didi2101 <diego.dasilva@epfl.ch>
2024-11-07 17:26:56 +0100

    Add a new data class WorkoutID to easily serialize and deserialize

--- app/src/main/java/com/android/sample/model/workout/Workout.kt
+++ app/src/main/java/com/android/sample/model/workout/Workout.kt
@@ -27,6 +27,11 @@ sealed class ExerciseDetail {
   data class RepetitionBased(val repetitions: Int) : ExerciseDetail()
 }
 
+@JsonClass(generateAdapter = true)
+data class WorkoutID(
+    val workoutid : String
+)
+
 enum class WorkoutType {
   BODY_WEIGHT,
   YOGA,
