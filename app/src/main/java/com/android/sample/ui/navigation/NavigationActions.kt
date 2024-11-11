ce5f068da93a4e472e6b9ac2e4145f8fe88bf85a
Shayan105 <119674649+Shayan105@users.noreply.github.com>
2024-11-11 21:40:00 +0100

    Correct the Navigation conflict

--- app/src/main/java/com/android/sample/ui/navigation/NavigationActions.kt
+++ app/src/main/java/com/android/sample/ui/navigation/NavigationActions.kt
@@ -25,9 +25,14 @@ object Route {
   const val WARMUP_WORKOUT = "Warmup workout"
   const val BODY_WEIGHT_WORKOUT = "BodyWeight workout"
   const val YOGA_WORKOUT = "Yoga workout"
+  const val FRIENDS = "Friends"
   const val CALENDAR = "Calendar"
   const val ADD_ACCOUNT = "Add Account"
   const val EDIT_ACCOUNT = "Edit Account"
+  const val BODY_WEIGHT_IMPORT = "BodyWeightImport"
+  const val CHOOSE_BODYWEIGHT = "Choose Bodyweight"
+  const val YOGA_IMPORT = "YogaImport"
+  const val CHOOSE_YOGA = "Choose Yoga"
 }
 
 object Screen {
@@ -50,9 +55,15 @@ object Screen {
   const val WARMUP_WORKOUT = "Warmup workout screen"
   const val BODY_WEIGHT_WORKOUT = "BodyWeight workout screen"
   const val YOGA_WORKOUT = "Yoga workout screen"
-
+  const val DAY_CALENDAR = "Day Calendar Screen"
   const val ADD_ACCOUNT = "Add Account Screen"
   const val EDIT_ACCOUNT = "Edit Account Screen"
+  const val FRIENDS = "Friends Screen"
+  const val ADD_FRIEND = "Add Friend Screen"
+  const val YOGA_IMPORT = "Yoga import screen"
+  const val CHOOSE_BODYWEIGHT = "Choose Bodyweight Screen"
+  const val CHOOSE_YOGA = "Choose Yoga Screen"
+  const val BODY_WEIGHT_IMPORT = "BodyWeight import screen"
 }
 
 data class TopLevelDestination(val route: String, val icon: ImageVector, val textId: String)
