a3af9381b06d51edf4293e04e2a21c20d58530a5
didi2101 <diego.dasilva@epfl.ch>
2024-11-21 18:49:45 +0100

    Implement the app in this Class and not in MainActivity (The intent for LocationService is here now)

--- app/src/main/java/com/android/sample/ui/googlemap/RunningScreen.kt
+++ app/src/main/java/com/android/sample/ui/googlemap/RunningScreen.kt
@@ -15,25 +15,34 @@ import com.android.sample.ui.composables.TopBar
 import com.android.sample.ui.navigation.NavigationActions
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Polyline
+import android.content.Context
+import android.content.Intent
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
-fun RunningScreen(onStartClick: () -> Unit, navigationActions: NavigationActions) {
+fun RunningScreen(navigationActions: NavigationActions) {
 
-  val currentLocation = LocationService.userLocation.collectAsState()
   val pathPoints = LocationService.pathPoints.collectAsState(initial = emptyList())
+    var stop: Boolean = false
   val cameraPositionState = LocationService.camera.collectAsState()
+
+    val context = LocalContext.current
 
   Scaffold(topBar = { TopBar(navigationActions, R.string.runningTitle) }) { innerPadding ->
     Column(
         modifier =
-            Modifier.fillMaxSize()
-                .padding(
-                    innerPadding) // Utilisez `innerPadding` pour éviter que la carte soit collée à
-                                  // la TopBar
-                .padding(horizontal = 16.dp)) {
+        Modifier
+            .fillMaxSize()
+            .padding(
+                innerPadding
+            ) // Utilisez `innerPadding` pour éviter que la carte soit collée à
+            // la TopBar
+            .padding(horizontal = 16.dp)) {
           Spacer(Modifier.height(32.dp))
 
-          Box(modifier = Modifier.weight(1f).fillMaxWidth()) {
+          Box(modifier = Modifier
+              .weight(1f)
+              .fillMaxWidth()) {
             GoogleMap(
                 modifier = Modifier.fillMaxSize(),
                 cameraPositionState = cameraPositionState.value) {
@@ -50,11 +59,30 @@ fun RunningScreen(onStartClick: () -> Unit, navigationActions: NavigationActions
 
           Spacer(modifier = Modifier.weight(0.5f))
 
-          Button(onClick = onStartClick, modifier = Modifier.fillMaxWidth().padding(top = 16.dp)) {
+          Button(onClick ={ LocationServiceManager.startLocationService(context) }, modifier = Modifier
+              .fillMaxWidth()
+              .padding(top = 16.dp)) {
             Text(text = "Start")
           }
 
           Spacer(Modifier.height(96.dp))
         }
   }
+}
+
+
+object LocationServiceManager {
+    fun startLocationService(context: Context) {
+        val intent = Intent(context, LocationService::class.java).apply {
+            action = LocationService.ACTION_START
+            context.startService(this)
+        }
+    }
+
+    fun stopLocationService(context: Context) {
+        val intent = Intent(context, LocationService::class.java).apply {
+            action = LocationService.ACTION_STOP
+            context.stopService(this)
+        }
+    }
 }
