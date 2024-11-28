30440c266e1153b0b72b0732d51da1c0096c2378
didi2101 <diego.dasilva@epfl.ch>
2024-11-28 20:18:03 +0100

    Reformat with ktfmt

--- app/src/main/java/com/android/sample/ui/composables/BottomBar.kt
+++ app/src/main/java/com/android/sample/ui/composables/BottomBar.kt
@@ -50,7 +50,10 @@ fun BottomBar(
             horizontalArrangement = Arrangement.SpaceAround,
             verticalAlignment = Alignment.CenterVertically) {
               LIST_OF_TOP_LEVEL_DESTINATIONS.forEach { destination ->
-                val isSelected = navigationActions.currentRoute() == (destination.route + " Screen") // navigationActions.currentRoute() returns the Screen....
+                val isSelected =
+                    navigationActions.currentRoute() ==
+                        (destination.route +
+                            " Screen") // navigationActions.currentRoute() returns the Screen....
 
                 // Animation for vertical offset
                 val offsetY by animateDpAsState(targetValue = if (isSelected) (-15).dp else 0.dp)
