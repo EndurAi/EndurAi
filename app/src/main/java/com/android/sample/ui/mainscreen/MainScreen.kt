f15988282c6eb345076f421359ad3f4e9546a318
Alec <alec.trachsel@epfl.ch>
2024-11-28 18:44:11 +0100

    Modify top bar and card from MainScreen.kt

--- app/src/main/java/com/android/sample/ui/mainscreen/MainScreen.kt
+++ app/src/main/java/com/android/sample/ui/mainscreen/MainScreen.kt
@@ -15,16 +15,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImagePainter.State.Empty.painter
 import coil.compose.rememberImagePainter
 import com.android.sample.R
 import com.android.sample.model.userAccount.UserAccount
@@ -37,9 +47,16 @@ import com.android.sample.ui.composables.BottomBar
 import com.android.sample.ui.navigation.NavigationActions
 import com.android.sample.ui.navigation.Screen
 import com.android.sample.ui.theme.Blue
+import com.android.sample.ui.theme.BlueWorkoutCard
+import com.android.sample.ui.theme.ContrailOne
 import com.android.sample.ui.theme.DarkBlue
 import com.android.sample.ui.theme.DarkBlue2
+import com.android.sample.ui.theme.DarkBlueTopBar1
+import com.android.sample.ui.theme.DarkBlueTopBar2
+import com.android.sample.ui.theme.DoubleArrow
 import com.android.sample.ui.theme.LightGrey
+import com.android.sample.ui.theme.Line
+import com.android.sample.ui.theme.OpenSans
 import com.android.sample.ui.theme.SoftGrey
 
 /**
@@ -78,10 +95,20 @@ fun MainScreen(
                   workout = workoutDisplayed,
                   profile = account?.profileImageUrl ?: "",
                   navigationActions = navigationActions)
+            Divider(
+                color = Line,
+                thickness = 0.8.dp,
+                modifier = Modifier.padding(horizontal = 25.dp, vertical = 3.dp).shadow(1.dp)
+            )
               QuickWorkoutSection(
                   navigationActions = navigationActions,
                   bodyWeightViewModel = bodyWeightViewModel,
                   yogaViewModel = yogaViewModel)
+            Divider(
+                color = Line,
+                thickness = 0.8.dp,
+                modifier = Modifier.padding(horizontal = 25.dp, vertical = 3.dp).shadow(1.dp)
+            )
               AchievementsSection(navigationActions = navigationActions)
             }
       },
@@ -97,42 +124,52 @@ fun MainScreen(
  */
 @Composable
 fun ProfileSection(account: UserAccount?, navigationActions: NavigationActions) {
-  Row(
-      modifier =
-          Modifier.fillMaxWidth()
-              .background(DarkBlue)
-              .padding(vertical = 12.dp)
-              .testTag("ProfileSection")) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .shadow(20.dp, clip = false)
+            .background(
+                brush = Brush.linearGradient(
+                colors = listOf(DarkBlueTopBar1, DarkBlueTopBar2), // Dégradé de DarkBlue à LightBlue
+                start = Offset(0f, 0f), // Début du dégradé (à gauche)
+                end = Offset(Float.POSITIVE_INFINITY, 0f))
+            )
+            .padding(vertical = 25.dp)
+    ) {
+        // Contenu de la Row (Image et Texte)
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(horizontal = 16.dp)) {
-              Image(
-                  painter = rememberImagePainter(data = account?.profileImageUrl ?: ""),
-                  contentDescription = "Profile",
-                  contentScale = ContentScale.Crop,
-                  modifier =
-                      Modifier.size(40.dp).clip(CircleShape).testTag("ProfilePicture").clickable {
-                        navigationActions.navigateTo(Screen.FRIENDS)
-                      })
-              Spacer(modifier = Modifier.width(8.dp))
-              Text(
-                  text = stringResource(id = R.string.welcome_message, account?.firstName ?: ""),
-                  style = MaterialTheme.typography.titleSmall.copy(fontSize = 20.sp),
-                  color = Color.White,
-                  modifier = Modifier.testTag("WelcomeText"))
-            }
-        Spacer(Modifier.weight(1f))
-
-        IconButton(
-            onClick = { navigationActions.navigateTo(Screen.SETTINGS) },
-            modifier = Modifier.padding(end = 12.dp).testTag("SettingsButton")) {
-              Icon(
-                  imageVector = Icons.Outlined.Settings,
-                  contentDescription = "Settings",
-                  tint = Color.White,
-                  modifier = Modifier.size(24.dp))
-            }
-      }
+            modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth()
+        ) {
+            Image(
+                painter = rememberImagePainter(data = account?.profileImageUrl ?: ""),
+                contentDescription = "Profile",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(54.dp)
+                    .clip(CircleShape)
+                    .testTag("ProfilePicture")
+                    .clickable { navigationActions.navigateTo(Screen.SETTINGS) } // temporary until the bottom bar implemented
+                    .shadow(4.dp)
+            )
+            Spacer(modifier = Modifier.width(20.dp))
+            Text(
+                text = stringResource(id = R.string.welcome_message, account?.firstName ?: ""),
+                style = MaterialTheme.typography.titleSmall.copy(fontSize = 24.sp, fontFamily = OpenSans,  fontWeight = FontWeight.Bold),
+                color = Color.White,
+                modifier = Modifier.testTag("WelcomeText")
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Image(
+                painter = painterResource(id = R.drawable.group),
+                contentDescription = "Group",
+                modifier = Modifier
+                    .size(30.dp)
+                    .testTag("Group")
+                    .clickable { navigationActions.navigateTo(Screen.FRIENDS)}
+            )
+        }
+    }
 }
 
 /**
@@ -148,16 +185,16 @@ fun WorkoutSessionsSection(
     profile: String,
     navigationActions: NavigationActions
 ) {
-  Column(modifier = Modifier.fillMaxWidth().padding(horizontal = 12.dp).testTag("WorkoutSection")) {
+  Column(modifier = Modifier.fillMaxWidth().padding(horizontal = 24.dp, vertical = 16.dp).testTag("WorkoutSection")) {
     Text(
-        text = "My workout sessions",
-        style = MaterialTheme.typography.titleSmall.copy(fontSize = 22.sp),
+        text = stringResource(id = R.string.WorkoutsTitle),
+        style = MaterialTheme.typography.titleSmall.copy(
+            fontSize = 24.sp,
+            fontFamily = OpenSans),
         modifier = Modifier.padding(vertical = 8.dp))
     Column(
         modifier =
             Modifier.fillMaxWidth()
-                .clip(RoundedCornerShape(12.dp))
-                .background(SoftGrey)
                 .height(300.dp)
                 .padding(8.dp)) {
           if (workout != null) {
@@ -174,38 +211,17 @@ fun WorkoutSessionsSection(
 
           Spacer(modifier = Modifier.weight(1f))
 
-          Box(
-              modifier =
-                  Modifier.fillMaxWidth()
-                      .clickable { navigationActions.navigateTo(Screen.SESSIONSELECTION) }
-                      .padding(vertical = 16.dp, horizontal = 12.dp)
-                      .height(48.dp)
-                      .background(Blue, RoundedCornerShape(20.dp))
-                      .testTag("NewWorkoutButton"),
-              contentAlignment = Alignment.Center) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                  Icon(
-                      imageVector = Icons.Outlined.Add,
-                      contentDescription = "New Workout",
-                      tint = Color.Black,
-                      modifier = Modifier.size(28.dp))
-                  Text(
-                      stringResource(id = R.string.NewWorkout),
-                      modifier = Modifier.padding(horizontal = 12.dp),
-                      style = MaterialTheme.typography.titleSmall.copy(fontSize = 16.sp))
-                }
-              }
-          Button(
-              onClick = { navigationActions.navigateTo(Screen.VIEW_ALL) },
-              modifier =
-                  Modifier.fillMaxWidth().padding(horizontal = 12.dp).testTag("ViewAllButton"),
-              colors =
-                  ButtonDefaults.buttonColors(
-                      containerColor = DarkBlue2, contentColor = Color.White)) {
-                Text(
-                    text = "View all",
-                    style = MaterialTheme.typography.titleSmall.copy(fontSize = 18.sp))
-              }
+        Image(
+            painter = rememberImagePainter(data = R.drawable.double_arrow),
+            contentDescription = "Double arrow",
+            colorFilter = ColorFilter.tint(DoubleArrow),
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+                .height(40.dp)
+                .testTag("Double Arrow")
+                .clickable { navigationActions.navigateTo(Screen.VIEW_ALL) }
+        )
         }
   }
 }
@@ -330,43 +346,52 @@ fun QuickWorkoutButton(
  */
 @Composable
 fun WorkoutCard(workout: Workout, profile: String, navigationActions: NavigationActions) {
-  Card(
-      shape = RoundedCornerShape(12.dp),
-      modifier =
-          Modifier.fillMaxWidth()
-              .padding(horizontal = 12.dp, vertical = 3.dp)
-              .clickable { /* Navigate to workout details or start workout */}
-              .testTag("WorkoutCard"),
-      colors = CardDefaults.cardColors(containerColor = Blue)) {
+    val shape = RoundedCornerShape(topStart = 40.dp, topEnd = 15.dp, bottomStart = 15.dp, bottomEnd = 40.dp)
+
+    Card(
+        shape = shape,
+        modifier = Modifier
+            .padding(horizontal = 30.dp)
+            .shadow(
+                elevation = 8.dp, shape = shape
+            )
+            .fillMaxWidth()
+            .clickable { /* Navigate to workout details or start workout */}
+            .testTag("WorkoutCard"),
+        colors = CardDefaults.cardColors(containerColor = BlueWorkoutCard)) {
+
         Row(
-            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween) {
               Column {
                 Text(
                     text = workout.name,
-                    style = MaterialTheme.typography.titleSmall.copy(fontSize = 17.sp))
-                Text(text = workout.description, style = MaterialTheme.typography.bodyMedium)
+                    style = MaterialTheme.typography.titleSmall.copy(fontSize = 19.sp, fontFamily = ContrailOne),
+                    modifier = Modifier.padding(horizontal = 10.dp))
+                  Spacer(modifier = Modifier.height(8.dp))
                 Image(
                     painter = rememberImagePainter(data = profile),
                     contentDescription = "Profile",
                     contentScale = ContentScale.Crop,
-                    modifier = Modifier.size(20.dp).clip(CircleShape))
+                    modifier = Modifier.size(28.dp).clip(CircleShape))
               }
               Image(
                   painter =
                       painterResource(
                           id =
                               when (workout) {
-                                is BodyWeightWorkout -> R.drawable.pushups
-                                is YogaWorkout -> R.drawable.yoga
-                                else -> R.drawable.dumbbell
+                                is BodyWeightWorkout -> R.drawable.dumbell_inner_shadow
+                                is YogaWorkout -> R.drawable.yoga_innershadow
+                                else -> R.drawable.running_innershadow
                               }),
                   contentDescription = "Workout Icon",
-                  modifier = Modifier.size(40.dp))
+                  modifier = Modifier.size(70.dp).padding(horizontal = 15.dp))
             }
       }
 }
+
+
 
 /**
  * Composable function that the button to navigate to the achievements
