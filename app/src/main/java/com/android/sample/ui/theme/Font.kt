b50aa167df47aec6cf3b738f9299d8d524a161b7
Alec <alec.trachsel@epfl.ch>
2024-11-28 10:42:37 +0100

    Add a new font : Open Sans and define it in the file Font.kt

---
+++ app/src/main/java/com/android/sample/ui/theme/Font.kt
@@ -0,0 +1,11 @@
+package com.android.sample.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.android.sample.R
+
+val OpenSans = FontFamily(
+    Font(R.font.open_sans_regular, FontWeight.Normal),
+    Font(R.font.open_sans_bold, FontWeight.Bold)
+)
