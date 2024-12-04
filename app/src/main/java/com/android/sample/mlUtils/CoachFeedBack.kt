7a19b2a7e46d876a9534e76132b96a47fb3a7077
Shayan105 <119674649+Shayan105@users.noreply.github.com>
2024-12-04 21:36:45 +0100

    Update CoachFeedBack.kt

--- app/src/main/java/com/android/sample/mlUtils/CoachFeedBack.kt
+++ app/src/main/java/com/android/sample/mlUtils/CoachFeedBack.kt
@@ -1,5 +1,5 @@
 package com.android.sample.mlUtils
 
-data class CoachFeedback(val commentSet : Set<JointFeedback>, val repOrDuration : Long, val repOrDurationUnit : String)
+data class CoachFeedback(val commentSet : Set<JointFeedback>, val successRate: Float, val repOrDuration : Long, val repOrDurationUnit : String)
 
 data class JointFeedback(val comment : String = "", val rate : Float = 0F)
