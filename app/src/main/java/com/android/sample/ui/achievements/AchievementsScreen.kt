2f158ad963b15bf756270b3254aaef404f10f507
Alec <alec.trachsel@epfl.ch>
2024-12-08 16:16:41 +0100

    Modify Statistics.kt to compute some basic statistics on a give list of workouts

--- app/src/main/java/com/android/sample/ui/achievements/AchievementsScreen.kt
+++ app/src/main/java/com/android/sample/ui/achievements/AchievementsScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import com.android.sample.model.achievements.Statistics
 import com.android.sample.model.achievements.StatisticsViewModel
 import com.android.sample.ui.composables.BottomBar
 import com.android.sample.ui.navigation.NavigationActions
@@ -16,13 +17,13 @@ import com.android.sample.ui.navigation.NavigationActions
 @Composable
 fun AchievementsScreen(navigationActions: NavigationActions, statisticsViewModel: StatisticsViewModel) {
     val workouts = statisticsViewModel.workoutStatistics.collectAsState().value
+    val statistics = Statistics(statisticsViewModel.workoutStatistics)
   Scaffold(
       modifier = Modifier.testTag("achievementsScreen"),
       content = { padding ->
           Column(modifier = Modifier.fillMaxSize().padding(padding)) {
-              Text(text = workouts.size.toString())
               if(workouts.isNotEmpty()) {
-                  Text(text = workouts[0].id)
+                  Text(text = statistics.getTotalWorkouts().toString())
               }
           } },
       bottomBar = { BottomBar(navigationActions) })
