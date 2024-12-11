643e464c78d330616b0905f042bb087d05d321f7
Siupahc <97094195+Siupahc@users.noreply.github.com>
2024-12-11 19:57:11 +0100

    Update CoachFeedbackScreen.kt

--- app/src/main/java/com/android/sample/ui/mlFeedback/CoachFeedbackScreen.kt
+++ app/src/main/java/com/android/sample/ui/mlFeedback/CoachFeedbackScreen.kt
@@ -1,11 +1,34 @@
 package com.android.sample.ui.mlFeedback
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.android.sample.R
 import com.android.sample.model.camera.CameraViewModel
+import com.android.sample.ui.composables.TopBar
 import com.android.sample.ui.navigation.NavigationActions
 
 @Composable
 fun CoachFeedbackScreen(navigationActions: NavigationActions, cameraViewModel: CameraViewModel) {
+    Scaffold(
+        topBar = {
+            TopBar(
+                title = R.string.coach_feedback_title,
+                navigationActions = navigationActions
+            )
+        },
+        content = {
+            pd ->
+                Column(
+                    modifier = Modifier.fillMaxSize().padding(pd)
+                ) {
 
+                }
+
+
+        }
+    )
 }
