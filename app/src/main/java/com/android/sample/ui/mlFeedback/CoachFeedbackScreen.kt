23becd4494c0e574592f28e72ec2585fb1687e32
Siupahc <97094195+Siupahc@users.noreply.github.com>
2024-12-11 19:03:31 +0100

    Create CoachFeedbackScreen.kt

---
+++ app/src/main/java/com/android/sample/ui/mlFeedback/CoachFeedbackScreen.kt
@@ -0,0 +1,11 @@
+package com.android.sample.ui.mlFeedback
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.android.sample.model.camera.CameraViewModel
+import com.android.sample.ui.navigation.NavigationActions
+
+@Composable
+fun CoachFeedbackScreen(navigationActions: NavigationActions, cameraViewModel: CameraViewModel) {
+
+}
