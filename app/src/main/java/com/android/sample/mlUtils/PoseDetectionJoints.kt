271a1ea2402054b2de3c9cf24e19c5a417915669
Shayan105 <119674649+Shayan105@users.noreply.github.com>
2024-12-12 19:07:01 +0100

    Add live detection for full body

--- app/src/main/java/com/android/sample/mlUtils/PoseDetectionJoints.kt
+++ app/src/main/java/com/android/sample/mlUtils/PoseDetectionJoints.kt
@@ -1,6 +1,17 @@
 package com.android.sample.mlUtils
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Canvas
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.google.mlkit.vision.pose.PoseLandmark
+import androidx.compose.foundation.Canvas
+import kotlin.reflect.full.companionObjectInstance
+import kotlin.reflect.full.memberProperties
 
 class PoseDetectionJoints {
   companion object {
@@ -51,5 +62,120 @@ class PoseDetectionJoints {
         Triple(PoseLandmark.RIGHT_HIP, PoseLandmark.LEFT_HIP, PoseLandmark.LEFT_KNEE)
     val RIGHT_OPPHIP_HIP_KNEE =
         Triple(PoseLandmark.LEFT_HIP, PoseLandmark.RIGHT_HIP, PoseLandmark.RIGHT_KNEE)
+
+
+
+    val ALL_JOINTS: Set<Triple<Int, Int, Int>> = setOf(
+      Triple(PoseLandmark.LEFT_ELBOW, PoseLandmark.LEFT_SHOULDER, PoseLandmark.LEFT_HIP),
+      Triple(PoseLandmark.RIGHT_ELBOW, PoseLandmark.RIGHT_SHOULDER, PoseLandmark.RIGHT_HIP),
+      Triple(PoseLandmark.LEFT_WRIST, PoseLandmark.LEFT_ELBOW, PoseLandmark.LEFT_SHOULDER),
+      Triple(PoseLandmark.RIGHT_WRIST, PoseLandmark.RIGHT_ELBOW, PoseLandmark.RIGHT_SHOULDER),
+      Triple(PoseLandmark.LEFT_SHOULDER, PoseLandmark.LEFT_HIP, PoseLandmark.LEFT_KNEE),
+      Triple(PoseLandmark.RIGHT_SHOULDER, PoseLandmark.RIGHT_HIP, PoseLandmark.RIGHT_KNEE),
+      Triple(PoseLandmark.LEFT_HIP, PoseLandmark.LEFT_KNEE, PoseLandmark.LEFT_ANKLE),
+      Triple(PoseLandmark.RIGHT_HIP, PoseLandmark.RIGHT_KNEE, PoseLandmark.RIGHT_ANKLE),
+      Triple(PoseLandmark.LEFT_ELBOW, PoseLandmark.LEFT_SHOULDER, PoseLandmark.RIGHT_SHOULDER),
+      Triple(PoseLandmark.RIGHT_ELBOW, PoseLandmark.RIGHT_SHOULDER, PoseLandmark.LEFT_SHOULDER),
+      Triple(PoseLandmark.RIGHT_HIP, PoseLandmark.LEFT_HIP, PoseLandmark.LEFT_KNEE),
+      Triple(PoseLandmark.LEFT_HIP, PoseLandmark.RIGHT_HIP, PoseLandmark.RIGHT_KNEE)
+    )
+
+
+    @Composable
+    fun DrawBody(
+      lastPose : List<MyPoseLandmark>,
+      wrongJoints: Set<Triple<Int,Int,Int>>,
+      cumulatedOffset : Offset = Offset.Zero,
+      jointColor : Color = Color.Blue,
+      jointStroke: Float = 5f,
+      lineColor : Color = Color.Magenta,
+      lineStroke : Float = 5f,
+      wrongJointColor : Color = Color.Blue,
+      wrongJointStroke: Float = 5f,
+      wronglineColor : Color = Color.Magenta,
+      wronglineStroke : Float = 5f,
+      modifier: Modifier = Modifier
+    ){
+
+      Canvas(modifier){
+
+        ALL_JOINTS.forEach { triple ->
+          //get the 3 points position
+          val a = lastPose[triple.first]
+          val b = lastPose[triple.second]
+          val c = lastPose[triple.third]
+          val isWrong = wrongJoints.contains(triple)
+          val roundColor = if (isWrong.not()) jointColor else wrongJointColor
+          val roundStroke = if (isWrong.not()) jointStroke else wrongJointStroke
+          val currentLineColor = if (isWrong.not()) lineColor else wronglineColor
+          val currentLineStroke = if (isWrong.not()) lineStroke else wronglineStroke
+
+
+          //draw the 3 points
+          val offset = 1f
+          drawCircle(
+            color =roundColor,
+            radius = roundStroke,
+            center = Offset(a.x * offset +cumulatedOffset.x, a.y * offset+cumulatedOffset.y)
+          )
+
+          drawCircle(
+            color =roundColor,
+            radius = roundStroke,
+            center = Offset(b.x * offset+cumulatedOffset.x, b.y * offset+cumulatedOffset.y)
+          )
+
+          drawCircle(
+            color =roundColor,
+            radius = roundStroke,
+            center = Offset(c.x * offset+cumulatedOffset.x, c.y * offset+cumulatedOffset.y)
+          )
+
+          // Draw a red line from a to b
+          drawLine(
+            color = currentLineColor,
+            start = Offset(a.x * offset+cumulatedOffset.x, a.y * offset+cumulatedOffset.y),
+            end = Offset(b.x * offset+cumulatedOffset.x, b.y * offset+cumulatedOffset.y),
+            strokeWidth = currentLineStroke
+          )
+
+// Draw a red line from b to c
+          drawLine(
+            color = currentLineColor,
+            start = Offset(b.x * offset+cumulatedOffset.x, b.y * offset+cumulatedOffset.y),
+            end = Offset(c.x * offset+cumulatedOffset.x, c.y * offset+cumulatedOffset.y),
+            strokeWidth = currentLineStroke
+          )
+
+
+        }
+
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   }
+
+
 }
