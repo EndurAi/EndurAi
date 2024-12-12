576cb982d422d5979cd90146c9d0afa51ea676f5
Shayan105 <119674649+Shayan105@users.noreply.github.com>
2024-12-12 20:22:03 +0100

    colours of the live feed back fixed

--- app/src/main/java/com/android/sample/mlUtils/PoseDetectionJoints.kt
+++ app/src/main/java/com/android/sample/mlUtils/PoseDetectionJoints.kt
@@ -1,17 +1,13 @@
 package com.android.sample.mlUtils
 
-import androidx.compose.foundation.layout.PaddingValues
+import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import com.google.mlkit.vision.pose.PoseLandmark
 import androidx.compose.foundation.Canvas
-import kotlin.reflect.full.companionObjectInstance
-import kotlin.reflect.full.memberProperties
+import com.android.sample.ui.composables.ImageComposable
 
 class PoseDetectionJoints {
   companion object {
@@ -65,7 +61,7 @@ class PoseDetectionJoints {
 
 
 
-    val ALL_JOINTS: Set<Triple<Int, Int, Int>> = setOf(
+    val ALL_JOINTS_LINKS: Set<Triple<Int, Int, Int>> = setOf(
       Triple(PoseLandmark.LEFT_ELBOW, PoseLandmark.LEFT_SHOULDER, PoseLandmark.LEFT_HIP),
       Triple(PoseLandmark.RIGHT_ELBOW, PoseLandmark.RIGHT_SHOULDER, PoseLandmark.RIGHT_HIP),
       Triple(PoseLandmark.LEFT_WRIST, PoseLandmark.LEFT_ELBOW, PoseLandmark.LEFT_SHOULDER),
@@ -81,70 +77,83 @@ class PoseDetectionJoints {
     )
 
 
+
     @Composable
     fun DrawBody(
       lastPose : List<MyPoseLandmark>,
-      wrongJoints: Set<Triple<Int,Int,Int>>,
+      wrongJointsLinks: Set<Triple<Int,Int,Int>>,
       cumulatedOffset : Offset = Offset.Zero,
       jointColor : Color = Color.Blue,
       jointStroke: Float = 5f,
       lineColor : Color = Color.Magenta,
       lineStroke : Float = 5f,
-      wrongJointColor : Color = Color.Blue,
+      wrongJointColor : Color = Color.Yellow,
       wrongJointStroke: Float = 5f,
-      wronglineColor : Color = Color.Magenta,
+      wronglineColor : Color = Color.Red,
       wronglineStroke : Float = 5f,
       modifier: Modifier = Modifier
     ){
+      var toastWasDisplayed  = false
 
       Canvas(modifier){
+        if(toastWasDisplayed.not()){
+          Toast.makeText("Drag to move the skeleton")
 
-        ALL_JOINTS.forEach { triple ->
+        }
+        val wrongJoints: Set<Int> = wrongJointsLinks.flatMap { listOf(it.first, it.second, it.third) }.toSet()
+        val wrongLines : MutableSet<Pair<Int, Int>> = mutableSetOf()
+        wrongJointsLinks.forEach { (a,b,c) ->
+          wrongLines += a to b
+          wrongLines += b to c
+        }
+
+        ALL_JOINTS_LINKS.forEach { triple ->
           //get the 3 points position
           val a = lastPose[triple.first]
           val b = lastPose[triple.second]
           val c = lastPose[triple.third]
-          val isWrong = wrongJoints.contains(triple)
-          val roundColor = if (isWrong.not()) jointColor else wrongJointColor
-          val roundStroke = if (isWrong.not()) jointStroke else wrongJointStroke
-          val currentLineColor = if (isWrong.not()) lineColor else wronglineColor
-          val currentLineStroke = if (isWrong.not()) lineStroke else wronglineStroke
+          val isWrongJoint1 = triple.first in wrongJoints
+          val isWrongJoint2 = triple.first in wrongJoints
+          val isWrongJoint3 = triple.first in wrongJoints
+
+          val line1IsWrong = (wrongLines.contains(triple.first to triple.second) || wrongLines.contains(triple.second to triple.first))
+          val line2IsWrong = (wrongLines.contains(triple.second to triple.third) || wrongLines.contains(triple.third to triple.second))
 
 
           //draw the 3 points
           val offset = 1f
           drawCircle(
-            color =roundColor,
-            radius = roundStroke,
+            color =if(isWrongJoint1) wrongJointColor else jointColor,
+            radius = if(isWrongJoint1) wrongJointStroke else jointStroke,
             center = Offset(a.x * offset +cumulatedOffset.x, a.y * offset+cumulatedOffset.y)
           )
 
           drawCircle(
-            color =roundColor,
-            radius = roundStroke,
+            color =if(isWrongJoint2) wrongJointColor else jointColor,
+            radius = if(isWrongJoint2) wrongJointStroke else jointStroke,
             center = Offset(b.x * offset+cumulatedOffset.x, b.y * offset+cumulatedOffset.y)
           )
 
           drawCircle(
-            color =roundColor,
-            radius = roundStroke,
+            color =if(isWrongJoint3) wrongJointColor else jointColor,
+            radius = if(isWrongJoint3) wrongJointStroke else jointStroke,
             center = Offset(c.x * offset+cumulatedOffset.x, c.y * offset+cumulatedOffset.y)
           )
 
           // Draw a red line from a to b
           drawLine(
-            color = currentLineColor,
+            color = if (line1IsWrong) wronglineColor else lineColor,
             start = Offset(a.x * offset+cumulatedOffset.x, a.y * offset+cumulatedOffset.y),
             end = Offset(b.x * offset+cumulatedOffset.x, b.y * offset+cumulatedOffset.y),
-            strokeWidth = currentLineStroke
+            strokeWidth = if (line1IsWrong) wronglineStroke else lineStroke
           )
 
 // Draw a red line from b to c
           drawLine(
-            color = currentLineColor,
+            color = if (line2IsWrong) wronglineColor else lineColor,
             start = Offset(b.x * offset+cumulatedOffset.x, b.y * offset+cumulatedOffset.y),
             end = Offset(c.x * offset+cumulatedOffset.x, c.y * offset+cumulatedOffset.y),
-            strokeWidth = currentLineStroke
+            strokeWidth = if (line2IsWrong) wronglineStroke else lineStroke
           )
 
 
