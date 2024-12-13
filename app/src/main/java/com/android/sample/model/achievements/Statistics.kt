74e207ac1aa3dd409442775c40fd74f192cd1af7
didi2101 <120500628+didi2101@users.noreply.github.com>
2024-12-13 01:07:34 +0100

    Implement a new method in Statistics.kt to obtain the calories of the current week of the user.

--- app/src/main/java/com/android/sample/model/achievements/Statistics.kt
+++ app/src/main/java/com/android/sample/model/achievements/Statistics.kt
@@ -3,6 +3,9 @@ package com.android.sample.model.achievements
 import com.android.sample.model.workout.WorkoutType
 import java.time.LocalDateTime
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.datetime.DayOfWeek
+import java.time.LocalDate
+import java.time.temporal.TemporalAdjusters
 
 /**
  * Class to compute some statistics based on a list of the workoutStatistics of the user.
@@ -12,14 +15,16 @@ import kotlinx.coroutines.flow.StateFlow
  */
 class Statistics(private val workoutStatisticsFlow: StateFlow<List<WorkoutStatistics>>) {
 
+  val today = LocalDateTime.now()
+
   /** Get the total number of workouts. */
   fun getTotalWorkouts(): Int {
     return workoutStatisticsFlow.value.size
   }
 
   /** Get a list of all workout dates. */
-  fun getDates(): List<LocalDateTime> {
-    return workoutStatisticsFlow.value.map { it.date }
+  fun getDates(): List<LocalDate> {
+    return workoutStatisticsFlow.value.map { it.date.toLocalDate() }
   }
 
   /** Get the total calories burnt across all workouts. */
@@ -31,4 +36,19 @@ class Statistics(private val workoutStatisticsFlow: StateFlow<List<WorkoutStatis
   fun getWorkoutsByType(): Map<WorkoutType, List<WorkoutStatistics>> {
     return workoutStatisticsFlow.value.groupBy { it.type }
   }
+
+  /** Get workouts calories of the week */
+  fun getCaloriesOfTheWeek(): Int {
+
+    val mondayOfTheWeek = today.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
+
+
+
+
+    val statsOfTheWeek = workoutStatisticsFlow.value.filter { stats -> stats.date.isAfter(mondayOfTheWeek.minusDays(1)) && stats.date.isBefore(today.plusWeeks(1)) }
+
+    return statsOfTheWeek.sumOf{it.caloriesBurnt}
+  }
+
+
 }
