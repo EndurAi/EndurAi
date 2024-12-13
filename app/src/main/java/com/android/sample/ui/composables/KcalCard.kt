01401393eaa6fb17cb236f0cb2a2e61bf73aeba4
didi2101 <120500628+didi2101@users.noreply.github.com>
2024-12-13 01:08:05 +0100

    Introduce a new Ui composable to display the Calories burnt of the user in a pretty card.

---
+++ app/src/main/java/com/android/sample/ui/composables/KcalCard.kt
@@ -0,0 +1,57 @@
+package com.android.sample.ui.composables
+
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.android.sample.ui.theme.Black
+import com.android.sample.ui.theme.NeutralGrey
+import com.android.sample.ui.theme.OpenSans
+import com.android.sample.ui.theme.White
+
+@Composable
+fun CaloriesDisplay(
+    calories: Int,
+) {
+    Surface(
+        modifier = Modifier
+            .width(300.dp)
+            .height(150.dp)
+            .shadow(elevation = 10.dp, shape = RoundedCornerShape(8.dp)),
+        color = White,
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text(
+                text = calories.toString(),
+                fontSize = 80.sp,
+                fontFamily = OpenSans,
+                fontWeight = FontWeight.SemiBold,
+                color = Black
+            )
+
+            Text(
+                text = "Kcal",
+                fontSize = 18.sp,
+                fontFamily = OpenSans,
+                fontWeight = FontWeight.SemiBold,
+                color = NeutralGrey
+            )
+        }
+    }
+}
+
