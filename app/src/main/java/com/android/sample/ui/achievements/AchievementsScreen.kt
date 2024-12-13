0f731caffcf2af40436b584737c8c31fa0e6251b
didi2101 <120500628+didi2101@users.noreply.github.com>
2024-12-13 01:18:39 +0100

    Implement The Achievements Screen to be able for the user to see his progress and consistancy with statistics.

--- app/src/main/java/com/android/sample/ui/achievements/AchievementsScreen.kt
+++ app/src/main/java/com/android/sample/ui/achievements/AchievementsScreen.kt
@@ -1,34 +1,114 @@
 package com.android.sample.ui.achievements
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import co.yml.charts.common.model.Point
+import com.android.sample.R
 import com.android.sample.model.achievements.Statistics
 import com.android.sample.model.achievements.StatisticsViewModel
-import com.android.sample.ui.composables.BottomBar
+import com.android.sample.ui.composables.CaloriesDisplay
+import com.android.sample.ui.composables.WeekCaloriesLineChart
+import com.android.sample.ui.composables.ToggleButtonAchievements
+import com.android.sample.ui.composables.TopBar
 import com.android.sample.ui.navigation.NavigationActions
+import com.android.sample.ui.theme.Black
+import com.android.sample.ui.theme.OpenSans
+import java.time.LocalDate
 
 @Composable
 fun AchievementsScreen(
     navigationActions: NavigationActions,
     statisticsViewModel: StatisticsViewModel
 ) {
-  val workouts = statisticsViewModel.workoutStatistics.collectAsState().value
-  val statistics = Statistics(statisticsViewModel.workoutStatistics)
-  Scaffold(
-      modifier = Modifier.testTag("achievementsScreen"),
-      content = { padding ->
-        Column(modifier = Modifier.fillMaxSize().padding(padding)) {
-          if (workouts.isNotEmpty()) {
-            Text(text = statistics.getTotalWorkouts().toString())
-          }
+    val statistics = Statistics(statisticsViewModel.workoutStatistics)
+
+    var isStatsSelected by remember { mutableStateOf(true) }
+
+    @Composable
+    fun StatisticsScreen(padding: PaddingValues) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Spacer(Modifier.weight(0.12f))
+
+
+            Text(
+                text = "Calories of the week",
+                fontSize = 28.sp,
+                fontFamily = OpenSans,
+                fontWeight = FontWeight.SemiBold,
+                color = Black
+            )
+
+            Spacer(Modifier.weight(0.03f))
+
+
+            CaloriesDisplay(calories = statistics.getCaloriesOfTheWeek())
+
+            val pointsData: List<Point> =
+                listOf(Point(0f, 562f), Point(1f, 1540f), Point(2f, 850f), Point(3f, 200f), Point(4f, 690f))
+            WeekCaloriesLineChart(pointsData)
+
+
+
+            Spacer(Modifier.weight(0.85f))
+
         }
-      },
-      bottomBar = { BottomBar(navigationActions) })
+
+    }
+
+    Scaffold(
+        modifier = Modifier.testTag("achievementsScreen"),
+        topBar = { TopBar(navigationActions, R.string.achievements) }
+    ) { padding ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxHeight(fraction = 0.2f)
+                .fillMaxWidth()
+                .padding(padding),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Spacer(Modifier.weight(0.5f))
+
+
+            ToggleButtonAchievements(onClick = {
+                isStatsSelected = !isStatsSelected; println("isStateSelected : " + isStatsSelected)
+            })
+
+            Spacer(Modifier.weight(0.5f))
+        }
+
+            when (isStatsSelected) {
+                true ->  StatisticsScreen(padding = padding)
+                false -> InfiniteCalendar(statistics, padding)
+            }
+    }
 }
+
+
