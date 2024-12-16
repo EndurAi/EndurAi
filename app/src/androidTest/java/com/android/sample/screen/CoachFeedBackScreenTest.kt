41f1f090e5de00427bf224a1fb376f5b2946b1fb
Shayan105 <119674649+Shayan105@users.noreply.github.com>
2024-12-16 22:35:28 +0100

    run ktfmt

--- app/src/androidTest/java/com/android/sample/screen/CoachFeedBackScreenTest.kt
+++ app/src/androidTest/java/com/android/sample/screen/CoachFeedBackScreenTest.kt
@@ -34,7 +34,12 @@ class CoachFeedBackScreenTest {
         .thenReturn(
             listOf(
                 CoachFeedback(
-                    emptySet(), 100f, 10, ExerciseFeedBackUnit.SECONDS, PlankExerciseCriterions)))
+                    emptySet(),
+                    100f,
+                    10,
+                    ExerciseFeedBackUnit.SECONDS,
+                    true,
+                    PlankExerciseCriterions)))
   }
 
   @Test
