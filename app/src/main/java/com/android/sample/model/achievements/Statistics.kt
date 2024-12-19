4ff2c769507a70e1534e0722fc51f8fbbd9190b2
diego <diego.dasilva@epfl.ch>
2024-12-19 01:31:04 +0100

    Implement new methods in Statistics to collect the calories and the distance of the week.

--- app/src/main/java/com/android/sample/model/achievements/Statistics.kt
+++ app/src/main/java/com/android/sample/model/achievements/Statistics.kt
@@ -37,7 +37,7 @@ class Statistics(private val workoutStatisticsFlow: StateFlow<List<WorkoutStatis
     return workoutStatisticsFlow.value.groupBy { it.type }
   }
 
-  /** Get workouts calories of the week */
+  /** Get total workouts calories of the week */
   fun getCaloriesOfTheWeek(): Int {
 
     val mondayOfTheWeek = today.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
@@ -50,4 +50,60 @@ class Statistics(private val workoutStatisticsFlow: StateFlow<List<WorkoutStatis
 
     return statsOfTheWeek.sumOf { it.caloriesBurnt }
   }
+
+  /** Get workouts calories of the week in a list */
+  fun getCaloriesOfTheWeekToList(): List<Double> {
+
+    val mondayOfTheWeek = today.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
+
+    val caloriesPerDay = MutableList(7) { 0.0 }
+
+    val statsOfTheWeek =
+      workoutStatisticsFlow.value.filter { stats ->
+        stats.date.isAfter(mondayOfTheWeek.minusDays(1)) &&
+                stats.date.isBefore(today.plusWeeks(1))
+      }
+    statsOfTheWeek.forEach { stats -> caloriesPerDay[stats.date.dayOfWeek.value - 1] += stats.caloriesBurnt.toDouble() }
+
+    return caloriesPerDay
+  }
+
+  /** Get the frequency of each workout type  */
+  fun getWorkoutTypeFrequency(): Map<WorkoutType, Double> {
+
+
+    val numberOfBodyweight = workoutStatisticsFlow.value.filter { stats -> stats.type == WorkoutType.BODY_WEIGHT }.size.toDouble()
+    val numberOfYoga = workoutStatisticsFlow.value.filter { stats -> stats.type == WorkoutType.YOGA }.size.toDouble()
+    val numberOfRunning = workoutStatisticsFlow.value.filter { stats -> stats.type == WorkoutType.RUNNING }.size.toDouble()
+
+
+
+
+    return listOf(
+      FrequencyWithWorkoutType(type = WorkoutType.BODY_WEIGHT, frequency = numberOfBodyweight),
+      FrequencyWithWorkoutType(type = WorkoutType.YOGA, frequency = numberOfYoga),
+      FrequencyWithWorkoutType(type = WorkoutType.RUNNING, frequency = numberOfRunning)).associate { it.type to it.frequency }
+  }
+
+  /** Get the distance of the week per day  */
+  fun getDistanceOfTheWeekPerDay(): List<Double> {
+
+
+    val mondayOfTheWeek = today.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
+
+    val distancePerDay = MutableList(7) { 0.0 }
+
+    val statsOfTheWeek =
+      workoutStatisticsFlow.value.filter{stats -> stats.type == WorkoutType.RUNNING}.filter { stats ->
+        stats.date.isAfter(mondayOfTheWeek.minusDays(1)) &&
+                stats.date.isBefore(today.plusWeeks(1))
+      }
+    statsOfTheWeek.forEach { stats -> distancePerDay[stats.date.dayOfWeek.value - 1] += stats.distance }
+
+    return distancePerDay
+  }
+
+
+
 }
+data class FrequencyWithWorkoutType( val type: WorkoutType, val frequency : Double )
